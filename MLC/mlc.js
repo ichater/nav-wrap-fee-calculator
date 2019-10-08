a1a936c6498393cc54a17f801668fa39b5f465c5
ichater <chaterizaak@gmail.com>
2019-10-08 16:50:12 +1100

    error on json database

--- MLC/mlc.js
+++ MLC/mlc.js
@@ -1,12 +1,39 @@
 const   express = require ("express"),
         app = express(),
-        bodyParser = require("body-parser");
+        bodyParser = require("body-parser"),
+        mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/feecalculator", { useNewUrlParser: true }); 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
+// app.locals.NavShares = require("./../Data.json/NavShares1.json");
+
+//Share Schema
+const shareSchema = new mongoose.Schema({
+    ASXCode: String,
+    Name: String,
+    Category: String
+});
+
+let Share = mongoose.model("Share", shareSchema);
+
+Share.find({}, function(err, listed){
+    if(err){
+        console.log("Not found yet")
+        console.log(err);
+    } else {
+        console.log("Found them!")
+        console.log(listed)
+    }
+});
 
 app.get("/", function(req, res){
 	res.render("home.ejs");
+});
+
+app.get("/feecalculator", function(req, res){
+	res.render("calculator.ejs");
 });
 
 app.listen(3000, function(){
