a380cfa6f55baddee25bf97440bfcdb6baa5baca
ichater <chaterizaak@gmail.com>
2019-10-21 10:48:42 +1100

    added delete

--- MLC/public/front.js
+++ MLC/public/front.js
@@ -11,32 +11,27 @@ $("#c2").on("keyup",(function(){
 		$(toDoText).val("");
         $("#totalBalance h2").text(dollars)       	
 }));
-//tiered structure based on total balance
-function S2(amount){
-    if(amount/100 < 200000){
-        $(".total").text('$' + roundToTwo(amount/100 *.004));
-    } 
-    else if(amount/100 > 200000 && amount < 500000){
-        $(".total").text('$' + roundToTwo(((amount/100) - 200000)*.0015) + 800)
-    } 
-    else{
-        $(".total").text('$' + roundToTwo((amount/100 - 500000)*.0003) + 1250)
-    }
-};
 
 function S22(amount){
     if((amount/100) < 200000){
-        $(".t1").text("$" + roundToTwo((amount/100) *.004));	
-    } 
+        if(amount/100 *.004 < 375){
+            $(".t1").text(375);
+        } else {
+            $(".t1").text(roundToTwo((amount/100) *.004));	
+    }}
     else if((amount/100) > 200000 && (amount/100) < 500000){
-        $(".t1").text("$800");
-        $(".t2").text("$" + roundToTwo(((amount/100) - 200000)* .0015));
+        $(".t1").text(800);
+        $(".t2").text(roundToTwo(((amount/100) - 200000)* .0015));
     } 
     else{
-        $(".t1").text("$800");
-        $(".t2").text("$450");
-        $(".t3").text("$" + roundToTwo(((amount/100) - 500000) * .0003));
-    }
+        $(".t1").text(800);
+        $(".t2").text(450);
+            if(((amount/100) - 500000) * .0003 + 1250 > 2400){
+            $(".t3").text(1150)
+            } else{
+                $(".t3").text(roundToTwo(((amount/100) - 500000) * .0003));
+            }
+}
 };
 
 $("#c2").on("keyup",(function(){
@@ -46,7 +41,8 @@ $("#c2").on("keyup",(function(){
         {style:"currency", currency:"AUD"});  
 	$(toDoText).val("");
     S22(toDoText)
-    S2(toDoText)
+    let total = (Number($(".t1").text()) + (Number($(".t2").text()) +(Number($(".t3").text()))))
+    $(".total").text(total);
 }));
 
 //Search through table
@@ -83,41 +79,77 @@ $("#c2").on("keyup",(function(){
 $(".select1").click(function(){
     let result = $("input[name='SMAcheck']:checked");
     result.each(function(){
-        // $(this).parent().siblings().text())
-   
+        //add investments  
         $("#SMATable").append(
             "<tr><th scope='col'>"  + $(this).parent().siblings(".SAPIR").text()+
             "</th><th scope='col'>" + $(this).parent().siblings(".SMA-name").text() +
             "</th><th scope='col'>" + $(this).parent().siblings(".smer").text() + 
-            "</th><th scope='col'>" + $(this).parent().siblings(".nabo").text() +
-            "</th><th> <input type=number placeholder='value'</th>"  
-        )
+            "</th><th scope='col'>" + $(this).parent().siblings(".nabo").text() + 
+            "</th><th> <input type=number placeholder='$'></th>" + 
+            "<th scope='col'> <input type=number placeholder='%'>  </th>"
+            + "<th scope='col'> <span class='del'> X </span> </th>"  
+            //delete added investment
+        ).delegate(".del", "click", function(){
+            $(this).parent().siblings().fadeOut(500, function(){
+                $(this).remove();
+            });
+            $(this).parent().fadeOut(500, function(){
+                $(this).remove();
+            });
+            event.stopPropagation();
+        }) 
+        $("input[name='SMAcheck']").prop("checked", false);
     })
 });
 //Funds
 $(".select2").click(function(){
     let result = $("input[name='fundcheck']:checked");
     result.each(function(){
+        //add TR for investment
         $("#fundTable").append(
             "<tr><th scope='col'>"  + $(this).parent().siblings(".MFAPIR").text()+
             "</th><th scope='col'>" + $(this).parent().siblings(".MFname").text() +
             "</th><th scope='col'>" + $(this).parent().siblings(".mer1").text() + 
-            "</th><th scope='col'>" + $(this).parent().siblings(".nabo1").text() +
-            "</th><th> <input type=number placeholder='value'</th>"  
-        )
+            "</th><th scope='col'>" + $(this).parent().siblings(".nabo1").text() + 
+            "</th><th> <input type=number placeholder='$'></th>" + 
+            "<th scope='col'> <input type=number placeholder='%'>  </th>"
+            + "<th scope='col'> <span class='del'> X </span> </th>"
+            //delete added investment
+        ).delegate(".del", "click", function(){
+            $(this).parent().siblings().fadeOut(500, function(){
+                $(this).remove();
+            });
+            $(this).parent().fadeOut(500, function(){
+                $(this).remove();
+            });
+            event.stopPropagation();
+        })
+        $("input[name='fundcheck']").prop("checked", false);
     })
 });
 //Shares
 $(".select3").click(function(){
     let result = $("input[name='ASXcheck']:checked");
     result.each(function(){  
+        //add TR for investment
         $("#ASXTable1").append(
             "<tr><th scope='col'>"  + $(this).parent().siblings(".code1").text()+
             "</th><th scope='col'>" + $(this).parent().siblings(".share-name").text() +
             "</th><th scope='col'>" + $(this).parent().siblings(".category").text() + 
-            "</th><th> <input type=number placeholder='value'</th>"
-            
-        )
+            "</th><th> <input type=number placeholder='$'></th>" + 
+            "<th scope='col'> <input type=number placeholder='%'>  </th>"
+            + "<th scope='col'> <span class='del'> X </span> </th>"  
+            //delete added table         
+        ).delegate(".del", "click", function(){
+                $(this).parent().siblings().fadeOut(500, function(){
+                    $(this).remove();
+                });
+                $(this).parent().fadeOut(500, function(){
+                    $(this).remove();
+                });
+                event.stopPropagation();
+        })
+        $("input[name='ASXcheck']").prop("checked", false);
     })
 });
 
@@ -139,8 +171,3 @@ $(".select3").click(function(){
   });
 
 
-  
-
-          
-
-  
